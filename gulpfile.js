1e883dcfb34df21b7ef50e88250a05197f3b10f9
ShaahidKhalpey <sk@Mys-MacBook-Pro.local>
2017-07-20 14:38:00 -0700

    added new new packages and started html and css

--- gulpfile.js
+++ gulpfile.js
@@ -12,23 +12,39 @@ gulp.task('scripts', function(){
 
 // Modify our default task method by passing an array of task names
 
-var gulp        = require('gulp');
-var watch       = require('gulp-watch');
-var eslint      = require('gulp-eslint');
-var browserSync = require('browser-sync').create();
+var gulp        = require('gulp'),
+    watch       = require('gulp-watch'),
+    eslint      = require('gulp-eslint').
+    sass        = require('gulp-sass'),
+    prettyError = require('gulp-prettyerror'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano     = require('gulp-cssnano'),
+    browserSync = require('browser-sync').create();
 
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+      .pipe(prettyError())
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'))
+});
 
 gulp.task('watch', function() {
-   gulp.watch('./js/*.js', ['scripts']);
-    
+    gulp.watch('./js/*.js', ['scripts']);
+    gulp.watch('./sass/*.scss', ['sass']);
 });
 
 gulp.task('lint', function() { 
     return gulp.src(['./js/*.js','!node_modules/**'])
         .pipe(eslint())
         .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
-})
+        .pipe(eslint.failAfterError())
+});
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -38,8 +54,8 @@ gulp.task('browser-sync', function() {
         }
     });
 
-    gulp.watch('./build/js/*.js').on('change', browserSync.reload);
+    gulp.watch(['./build/js/*.js', './build/css/*.css' ]).on('change', browserSync.reload);
 
 });
 
-gulp.task('default', ['watch', 'lint', 'scripts', 'browser-sync']);
+gulp.task('default', ['watch', 'browser-sync']);
