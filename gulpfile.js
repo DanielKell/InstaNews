8560adb087e7196b35949eeace97f3a2b2e1787d
ShaahidKhalpey <sk@Mys-MacBook-Pro.local>
2017-07-20 19:04:26 -0700

    added nyt api

--- gulpfile.js
+++ gulpfile.js
@@ -3,58 +3,63 @@ var gulp = require('gulp'); // Load Gulp!
 var uglify = require('gulp-uglify'),
     rename = require('gulp-rename');
 
-gulp.task('scripts', function(){
-  gulp.src('./js/*.js') // What files do we want gulp to consume?
-    .pipe(uglify()) // Call the uglify function on these files
-    .pipe(rename({ extname: '.min.js' })) // Rename the uglified file - renaming the extention name
-    .pipe(gulp.dest('./build/js/')) // Where do we put the result. The destianation of where the file should go
-});
+
 
 // Modify our default task method by passing an array of task names
 
-var gulp        = require('gulp'),
-    watch       = require('gulp-watch'),
-    eslint      = require('gulp-eslint').
-    sass        = require('gulp-sass'),
+var watch = require('gulp-watch'),
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
     prettyError = require('gulp-prettyerror'),
     autoprefixer = require('gulp-autoprefixer'),
-    cssnano     = require('gulp-cssnano'),
+    cssnano = require('gulp-cssnano'),
     browserSync = require('browser-sync').create();
 
-gulp.task('sass', function() {
-   gulp.src('./sass/style.scss')
-      .pipe(prettyError())
-      .pipe(sass())
-      .pipe(autoprefixer({
-         browsers: ['last 2 versions']
-      }))
-      .pipe(gulp.dest('./build/css'))
-      .pipe(cssnano())
-      .pipe(rename('style.min.css'))
-      .pipe(gulp.dest('./build/css'))
+
+gulp.task('scripts', function () {
+    gulp.src('./js/*.js') // What files do we want gulp to consume?
+        .pipe(uglify()) // Call the uglify function on these files
+        .pipe(rename({
+            extname: '.min.js'
+        })) // Rename the uglified file - renaming the extention name
+        .pipe(gulp.dest('./build/js/')) // Where do we put the result. The destianation of where the file should go
 });
 
-gulp.task('watch', function() {
+
+gulp.task('sass', function () {
+    gulp.src('./sass/style.scss')
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./build/css'))
+        .pipe(cssnano())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./build/css'))
+});
+
+gulp.task('watch', function () {
     gulp.watch('./js/*.js', ['scripts']);
     gulp.watch('./sass/*.scss', ['sass']);
 });
 
-gulp.task('lint', function() { 
-    return gulp.src(['./js/*.js','!node_modules/**'])
+gulp.task('lint', function () {
+    return gulp.src(['./js/*.js', '!node_modules/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError())
 });
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
 
-    gulp.watch(['./build/js/*.js', './build/css/*.css' ]).on('change', browserSync.reload);
+    gulp.watch(['./build/js/*.js', './build/css/*.css']).on('change', browserSync.reload);
 
 });
 
--- js/script.js
+++ js/script.js
@@ -1,2 +1,52 @@
-console.log("hello 3");
+$(document).ready(function() {
+ 
 
+
+  $('.section-selector').change(function () {
+
+    
+
+    var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+    url += '?' + $.param({
+      'api-key': "d4dad082902b43c791328b91dbc82d5a"
+    });
+
+    $.ajax({
+        url: url,
+        method: 'GET', 
+      })
+      .done(function (data) {
+        
+        
+        //console.log(data.results);
+
+        $('.news').remove();
+        
+        var articles = '';
+
+
+        $.each(data.results, function (key, value) {
+          
+          if (value.multimedia.length) {
+
+            console.log(value.multimedia[4].url);
+
+            articles += '<div class="news">';
+            articles += '<img src="' + value.multimedia[4].url + '"/>';
+            articles += '</div>';
+
+          } 
+          else {
+            console.log('no images found');
+          }
+        });
+
+        $('.articles').append(articles);
+
+      }).fail(function (err) {
+        throw err;
+      });
+
+  });
+
+});
