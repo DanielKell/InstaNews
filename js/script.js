c420214ff52e8fc90dfc7a615c2b1e549fb68961
ShaahidKhalpey <sk@Mys-MacBook-Pro.local>
2017-07-21 18:03:38 -0700

    updated js and scss files

--- js/script.js
+++ js/script.js
@@ -1,51 +1,48 @@
-$(document).ready(function() {
- 
+$(document).ready(function () {
 
 
   $('.section-selector').change(function () {
 
-    
+    var input = $('#selection').val();
 
-    var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+    var url = "https://api.nytimes.com/svc/topstories/v2/" + input + ".json";
     url += '?' + $.param({
       'api-key': "d4dad082902b43c791328b91dbc82d5a"
     });
 
     $.ajax({
         url: url,
-        method: 'GET', 
+        method: 'GET',
       })
       .done(function (data) {
-        
-        
-        //console.log(data.results);
 
         $('.news').remove();
-        
+
         var articles = '';
 
+        var articlesWithImages = data.results.filter(function (article) {
+          return article.multimedia.length
+        }).slice(0, 12);    
 
-        $.each(data.results, function (key, value) {
-          
-          if (value.multimedia.length) {
+        console.log(data);
 
-            console.log(value.multimedia[4].url);
+        $.each(articlesWithImages, function (key, value) {
 
-            articles += '<div class="news">';
-            articles += '<img src="' + value.multimedia[4].url + '"/>';
-            articles += '</div>';
+          articles += '<div class="news">';
+          articles += '<a href="' + value.url + '" target="_blank">' + '<img src="' + value.multimedia[1].url + '"/>' + '</a>';
+          articles += '<p>' + value.abstract + '</p>'
+          articles += '</div>';
 
-          } 
-          else {
-            console.log('no images found');
-          }
+
         });
 
-        $('.articles').append(articles);
 
-      }).fail(function (err) {
+        $('.articles').append(articles);
+      })
+      .fail(function (err) {
         throw err;
       });
+
 
   });
 
